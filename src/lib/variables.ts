a3b921b75dd709ea5bbf4c6d896cd083591654a1
Parames Ajhan <parames.ajh@gmail.com>
2025-03-06 16:58:30 +0700

    feat: change footer copyright
    change copyright text to made with love by maxexq

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -2,20 +2,21 @@
 
 export const GLOBAL = {
   // Site metadata
-  username: "Zaggonaut",
+  username: "maxexq",
   rootUrl: "https://zaggonaut.dev",
   shortDescription: "Retro-Inspired Theme &<br /> Built for Astro",
-  longDescription: "Zaggonaut is a retro-inspired theme for Astro, built using TypeScript, TailwindCSS, and Astro.",
-  
+  longDescription:
+    "Zaggonaut is a retro-inspired theme for Astro, built using TypeScript, TailwindCSS, and Astro.",
+
   // Social media links
   githubProfile: "https://github.com/RATIU5/zaggonaut",
   twitterProfile: "https://twitter.com/",
-  
+
   // Common text names used throughout the site
   articlesName: "Articles",
   projectsName: "Projects",
   viewAll: "View All",
-  
+
   // Common descriptions used throughout the site
   noArticles: "No featured articles yet.",
   noProjects: "No featured projects yet.",
@@ -23,12 +24,15 @@ export const GLOBAL = {
   // Blog metadata
   blogTitle: "My Thoughts & Takes",
   blogShortDescription: "Practical wisdom, unfiltered thoughts, and hot takes.",
-  blogLongDescription: "Web development, tech trends, and the occasional programming mishap.",
+  blogLongDescription:
+    "Web development, tech trends, and the occasional programming mishap.",
 
   // Project metadata
   projectTitle: "Projects and Code",
-  projectShortDescription: "A list of my web development projects and developer tools.",
-  projectLongDescription: "All of my projects, including both frontend and full-stack applications.",
+  projectShortDescription:
+    "A list of my web development projects and developer tools.",
+  projectLongDescription:
+    "All of my projects, including both frontend and full-stack applications.",
 
   // Profile image
   profileImage: "michael-dam-unsplash.webp",
@@ -38,5 +42,5 @@ export const GLOBAL = {
     home: "/",
     projects: "/projects",
     blog: "/blog",
-  }
+  },
 };
